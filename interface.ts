4ca354082787e045227ae2544b32eaec5bad4bd7
Ninntexx <152378504+Ninntexx@users.noreply.github.com>
2024-04-22 17:01:37 +0200

    add isMenu interface

---
+++ interface.ts
@@ -0,0 +1,11 @@
+interface isMenu {
+    name: string;
+    image?: string;
+    description?: string;
+    season: string;
+    diet: string;
+    menu: string;
+    taste: string;
+    temperature: string;
+    healthy: string;
+}
