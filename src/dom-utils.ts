a8facff4ac4b67a305feec24e4101f869ddba1c0
Ninntexx <152378504+Ninntexx@users.noreply.github.com>
2024-04-24 14:40:06 +0200

    add htmlElement exports

---
+++ src/dom-utils.ts
@@ -0,0 +1,4 @@
+export const page1 = document.querySelector('#page1') as HTMLFormElement;
+export const modal = document.querySelector('.modal') as HTMLFormElement;
+export const modalContent = document.querySelector('.modalContent') as HTMLFormElement;
+export const addMenu = document.querySelector('#addMenu') as HTMLInputElement;
