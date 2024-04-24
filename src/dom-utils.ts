a39707503d03d24b2e3a1cc47c0511d377250851
Ninntexx <152378504+Ninntexx@users.noreply.github.com>
2024-04-24 18:13:31 +0200

    useless exports

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,6 +1,3 @@
 export const page1 = document.querySelector('#page1') as HTMLFormElement;
 export const page2 = document.querySelector('#page2') as HTMLFormElement;
 export const modal = document.querySelector('.modal') as HTMLFormElement;
-export const modalContent = document.querySelector('.modalContent') as HTMLFormElement;
-export const addMenu = document.querySelector('#addMenu') as HTMLInputElement;
-export const next = document.querySelector('#next') as HTMLInputElement;
