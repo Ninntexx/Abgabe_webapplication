379870c1bd8400d0cad0b7661e825429fba1ecb8
Ninntexx <152378504+Ninntexx@users.noreply.github.com>
2024-04-24 14:39:16 +0200

    add hide modal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-
+import { page1, modal, modalContent, addMenu } from './dom-utils';
+import { isMenu } from './interface';
 import './styles/styles.css';
+
+function hideForm() {
+    modal.style.display = 'none';
+}hideForm();
 
 
 
