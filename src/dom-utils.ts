96a0e4eb117d81907212fcd886d79ef815def36f
Ninntexx <152378504+Ninntexx@users.noreply.github.com>
2024-04-25 17:09:44 +0200

    removing the form page1/page2 from index.html

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,3 +1,4 @@
 export const page1 = document.querySelector('#page1') as HTMLFormElement;
 export const page2 = document.querySelector('#page2') as HTMLFormElement;
 export const modal = document.querySelector('.modal') as HTMLFormElement;
+export const addMenu = document.querySelector('#addMenu') as HTMLInputElement;
